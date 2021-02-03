9284c8ce0ff9017e325cd8d0f2731bbfc81f13eb
vadimk7 <er.coooool@gmail.com>
2021-02-03 09:41:55 +0300

    fix(event-source): simplify NgEventSource

--- projects/event-source/src/classes/ng-event-source.ts
+++ projects/event-source/src/classes/ng-event-source.ts
@@ -1,40 +1,42 @@
-import {Observable} from 'rxjs';
+import {Observable, Subscriber} from 'rxjs';
 import {NgMessageEvent} from '../interfaces/message-event';
-import {SubscribersStore} from './subscribers-store';
 
 export class NgEventSource<T> extends Observable<NgMessageEvent<T>> {
-    private store = new SubscribersStore<Event>();
+    private subscribers = new Set<Subscriber<Event>>();
     private eventSource: EventSource | null = null;
 
     constructor(private url: string, private eventSourceInit?: EventSourceInit) {
         super(subscriber => {
-            this.store.add(subscriber);
+            this.subscribers.add(subscriber);
 
-            return () => this.store.delete(subscriber);
+            if (this.eventSource === null) {
+                this.connect();
+            }
+
+            return () => {
+                this.subscribers.delete(subscriber);
+
+                if (this.subscribers.size === 0) {
+                    this.close();
+                }
+            };
         });
-
-        this.store.change$.subscribe(subscribers =>
-            subscribers.size > 0 ? this.connect() : this.disconnect(),
-        );
     }
 
     private connect() {
-        if (this.eventSource) {
-            return;
-        }
-
         this.eventSource = new EventSource(this.url, this.eventSourceInit);
-        this.eventSource.onopen = event => this.store.forEach(s => s.next(event));
-        this.eventSource.onmessage = event => this.store.forEach(s => s.next(event));
-        this.eventSource.onerror = event => this.store.forEach(s => s.error(event));
+        this.eventSource.onopen = event =>
+            this.subscribers.forEach(subscriber => subscriber.next(event));
+        this.eventSource.onmessage = event =>
+            this.subscribers.forEach(subscriber => subscriber.next(event));
+        this.eventSource.onerror = event =>
+            this.subscribers.forEach(subscriber => subscriber.error(event));
     }
 
-    private disconnect() {
-        if (this.eventSource === null) {
-            return;
+    private close() {
+        if (this.eventSource) {
+            this.eventSource.close();
+            this.eventSource = null;
         }
-
-        this.eventSource.close();
-        this.eventSource = null;
     }
 }
